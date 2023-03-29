9e6755ec911f1f32fe606ae3dbdf05220bc8a0ab
kishoredevops1 <96432037+kishoredevops1@users.noreply.github.com>
2023-03-30 00:18:26 +0530

    Create install.sh

---
+++ dynamic_inventory_ansible/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install software-properties-common -y 
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get install ansible -y
+ansible-galaxy collection install amazon.aws
+sudo apt-get install python3-pip
+pip3 install boto3
